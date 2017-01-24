13a7395ce0f1da1af7c6a1978c6fdabd74d93764
Kristin Caron <krissycaron@gmail.com>
2017-01-23 21:35:51 -0600

    added sonnet

--- sonnet.js
+++ sonnet.js
@@ -5,7 +5,50 @@ var numberFive = parseInt(five);
 
 //parsefloat = integers with decimals
 
-var firstName = "Kristin"
-var lastName = "Caron"
-var fullName = firstName + lastName;
-console.log(fullName);
+
+
+//String CanCatination
+// var firstName = "Kristin"
+// var lastName = "Caron"
+// var fullName = firstName + lastName;
+// console.log(fullName);
+
+
+//quotes how to write one using "\"
+
+// var quote = "\"Life is good!\""
+
+// console.log ("length: ", quote.length);
+
+// console.log ("Period at index: ", quote.indexOf("!"));
+
+
+// var newPhrase = quote.replace("good", "purple")
+// console.log("quote with replaced word: ", newPhrase)
+
+// var newPhrase = quote.replace(/i/g, "z")
+// console.log("quote with letters replaced", newPhrase)
+
+// var phrase = "Hey, look at me!"
+
+
+// var element = document.getElementById("container");
+// console.log(document);
+
+
+// element.innerHTML = "<b>hello</b>"
+
+
+var element = document.getElementById("sonnet")
+
+var sonnet =element.innerHTML;
+
+//.innerHTML only grabs the text in the html div 
+
+console.log(sonnet);
+
+console.log ("length: ", sonnet.length);
+
+
+
+
