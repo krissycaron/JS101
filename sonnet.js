331cfcbdec064b5366d4d6c23c187a1ca910a8c3
Kristin Caron <krissycaron@gmail.com>
2017-01-23 20:49:23 -0600

    string cancatination

---
+++ sonnet.js
@@ -0,0 +1,11 @@
+//string Manipulation
+
+var five = "5"; //"5"
+var numberFive = parseInt(five); 
+
+//parsefloat = integers with decimals
+
+var firstName = "Kristin"
+var lastName = "Caron"
+var fullName = firstName + lastName;
+console.log(fullName);
