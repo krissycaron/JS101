31328b6ad06564e2fe8ef84558c12cef9330b544
Kristin Caron <krissycaron@gmail.com>
2017-01-24 19:22:16 -0600

    switch lesson

---
+++ day2.js
@@ -0,0 +1,21 @@
+//switch statement
+
+var day = "Friday";
+
+switch(day) {
+
+	case "Monday":
+		console.log("BOOOOO");
+		break;
+	case "Friday":
+		console.log("Partay!");
+		break;
+	default:
+		console.log("not Monday");
+}
+
+
+/*use a switch to match different strings or numbers "if/else" 
+statements are the most efficent but sometimes this is cleaner */
+
+
